c48936405d8b6e80218e83640266cac768675739
i_hall <isabel.m.hall@gmail.com>
2022-02-14 17:35:02 +0000

    iterate through directory of json files and add to mongodb client

--- src/db.py
+++ src/db.py
@@ -2,8 +2,10 @@ import pymongo
 import os
 import json
 from pathlib import Path
+from tqdm import tqdm
 
 def get_db_details():
+    # Initialise a mongoDB client
     mongo_url = os.environ["MONGO_URL"]
     mongo_username = os.environ["MONGO_INITDB_ROOT_USERNAME"]
     mongo_password = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
@@ -15,27 +17,36 @@ def get_db_details():
     db = client.fhir_data
     return db
 
-def add_patients(db, file_path):
-    path = Path(file_path)
-    with path.open(mode="r") as f:
+def add_collection(db, file_path):
+
+    # Open a json file containing fhir resources, of one resource type
+    print(file_path)
+    with file_path.open(mode="r") as f:
         resources = json.load(f)
 
-    items_to_save = []
+    records_to_save = []
 
+    # Extract the resource data and add the fullUrl, removes one layer of nesting
     for resource in resources:
         res = resource["resource"]
         url = resource["fullUrl"]
         res["fullUrl"] = url
-        items_to_save.append(res)
+        records_to_save.append(res)
     
-    name = path.stem
-    inserted = db[name].insert_many(items_to_save)
+    # Insert into mongoDB client
+    name = file_path.stem
+    inserted = db[name].insert_many(records_to_save)
+    return inserted
 
-    pats = list(db[name].find({}))
-    print(pats[0])
+def add_many_collections(db, path_dir):
+    path = Path(path_dir)
+    file_paths = list(path.glob("*.json"))
+    for p in tqdm(file_paths, total = len(file_paths)):
+        inserted = add_collection(db, p)
+
 
 if __name__ == "__main__":
     db = get_db_details()
-    file_path = "../validated_resources/Patient.json"
+    file_path = "../validated_resources/"
 
-    add_patients(db, file_path)
+    add_many_collections(db, file_path)
