1f6878eb9773ba857eb683682f30b6015cc0c01a
i_hall <isabel.m.hall@gmail.com>
2022-02-14 13:19:36 +0000

    generalising input filepath with fire

--- process_bundles.py
+++ process_bundles.py
@@ -1,38 +1,52 @@
 import json
 from pathlib import Path
-
-with open("/home/issie/code/exa-data-eng-assessment/data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json", "r") as f:
-    bundle = json.load(f)
-    entries = bundle["entry"]
-print(len(entries))
-
-resource_types = set()
-output_dir = Path("resources")
-if not output_dir.exists():
-    output_dir.mkdir()
+import fire
 
 
-for resource in entries:
-    resource_type = resource["resource"]["resourceType"]
-    resource_types.add(resource_type)
-    resource_file_path = output_dir / f"{resource_type}.json"
+def process_bundles(json_file_path):
+    """
+    Takes an individual json file containing FHIR data
+    Separates it based on resource types and saves these into json files
+    """
 
-    if resource_file_path.exists():
+    # Open the FHIR json file
+    with open(json_file_path, "r") as f:
+        bundle = json.load(f)
+        entries = bundle["entry"]
 
-        # get existing records of this resource
-        with resource_file_path.open(mode="r") as f:
-            records = json.load(f)
-    
-        # add stuff to it
-        records.append(resource)
 
-    else:
-        records = [resource]
+    resource_types = set()
+
+    # Make a directory to save the processed files
+    output_dir = Path("resources")
+    if not output_dir.exists():
+        output_dir.mkdir()
+
+    # Group different resource types together and save them into distinct json files
+    for resource in entries:
+        resource_type = resource["resource"]["resourceType"]
+        resource_types.add(resource_type)
+        resource_file_path = output_dir / f"{resource_type}.json"
+
+        if resource_file_path.exists():
+
+            # get existing records of this resource
+            with resource_file_path.open(mode="r") as f:
+                records = json.load(f)
         
-    # save it back to disk
-    with resource_file_path.open(mode="w") as f:
-        json.dump(records, f)
+            # add stuff to it
+            records.append(resource)
+
+        else:
+            records = [resource]
+            
+        # save it to disk
+        with resource_file_path.open(mode="w") as f:
+            json.dump(records, f)
 
 
 
-print(resource_types)
+    #print(resource_types)
+
+if __name__ == "__main__":
+    fire.Fire(process_bundles)
--- validate_resources.py
+++ validate_resources.py
@@ -1,57 +1,61 @@
 import json
 from pathlib import Path
 from fhir.resources import construct_fhir_element
+import fire
 
+def validate_resources():
 # Create output directory
-output_dir = Path("validated_resources")
-if not output_dir.exists():
-    output_dir.mkdir()
+    output_dir = Path("validated_resources")
+    if not output_dir.exists():
+        output_dir.mkdir()
 
-input_dir = Path("resources")
+    input_dir = Path("resources")
 
-# For each file in resources directory validate that they contain valid FHIR resources
-for p in input_dir.iterdir():
-    #print(p)
+    # For each file in resources directory validate that they contain valid FHIR resources
+    for p in input_dir.iterdir():
 
-    with p.open(mode="r") as f:
-        resource_list = json.load(f)
-        #print(len(resource_list))
-
-        for resource in resource_list:
-            resource_type = resource["resource"]["resourceType"]
-
-            # Validate that the resource has a valid FHIR resource type
-            try:
-                fhir_element = construct_fhir_element(resource_type, resource["resource"])
-
-                # Save the validated records to a new json file
-                output_path = output_dir / p.parts[1]
-                
-                if output_path.exists():
-                    with output_path.open(mode="r") as f:
-                        records = json.load(f)
-
-                    records.append(resource)
-
-                else:
-                    records = [resource]
-
-                with output_path.open(mode="w") as f:
-                    json.dump(records, f)
+        # Open each json file containing a list of resources of a specific type
+        with p.open(mode="r") as f:
+            resource_list = json.load(f)
 
 
-            except ValueError:
-                # If invalid will save it to a separate invalid resources file so it can be reviewed
-                invalid_path_file = output_dir / "invalid_resources.json"
+            for resource in resource_list:
+                resource_type = resource["resource"]["resourceType"]
 
-                if invalid_path_file.exists():
-                    with invalid_path_file.open(mode="r") as f:
-                        records = json.load(f)
+                # Validate that the resource has a valid FHIR resource type
+                try:
+                    fhir_element = construct_fhir_element(resource_type, resource["resource"])
 
-                    records.append(resource)
+                    # Save the validated records to a new json file
+                    output_path = output_dir / p.parts[1]
+                    
+                    # load the target file if it already exists
+                    if output_path.exists():
+                        with output_path.open(mode="r") as f:
+                            records = json.load(f)
 
-                else:
-                    records = [resource]
+                        records.append(resource)
 
-                with invalid_path_file.open(mode="w") as f:
-                    json.dump(records, f)
+                    else:
+                        records = [resource]
+
+                    # Save the updated json file, containing only valid FHIR resources
+                    with output_path.open(mode="w") as f:
+                        json.dump(records, f)
+
+
+                except ValueError:
+                    # If invalid will save it to a separate invalid resources file so it can be reviewed
+                    invalid_path_file = output_dir / "invalid_resources.json"
+
+                    if invalid_path_file.exists():
+                        with invalid_path_file.open(mode="r") as f:
+                            records = json.load(f)
+
+                        records.append(resource)
+
+                    else:
+                        records = [resource]
+
+                    with invalid_path_file.open(mode="w") as f:
+                        json.dump(records, f)
