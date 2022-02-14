7204452c93620dd60b4d6a6387006f340dd9ca6d
i_hall <isabel.m.hall@gmail.com>
2022-02-14 12:58:04 +0000

    validate_resources MVP

--- validate_resources.py
+++ validate_resources.py
@@ -1,6 +1,5 @@
 import json
 from pathlib import Path
-from fhir.resources.patient import Patient
 from fhir.resources import construct_fhir_element
 
 # Create output directory
@@ -12,15 +11,47 @@ input_dir = Path("resources")
 
 # For each file in resources directory validate that they contain valid FHIR resources
 for p in input_dir.iterdir():
+    #print(p)
 
-
-    with open(p, "r") as f:
+    with p.open(mode="r") as f:
         resource_list = json.load(f)
+        #print(len(resource_list))
 
         for resource in resource_list:
             resource_type = resource["resource"]["resourceType"]
 
+            # Validate that the resource has a valid FHIR resource type
             try:
                 fhir_element = construct_fhir_element(resource_type, resource["resource"])
-            except ValueError:
+
+                # Save the validated records to a new json file
+                output_path = output_dir / p.parts[1]
                 
+                if output_path.exists():
+                    with output_path.open(mode="r") as f:
+                        records = json.load(f)
+
+                    records.append(resource)
+
+                else:
+                    records = [resource]
+
+                with output_path.open(mode="w") as f:
+                    json.dump(records, f)
+
+
+            except ValueError:
+                # If invalid will save it to a separate invalid resources file so it can be reviewed
+                invalid_path_file = output_dir / "invalid_resources.json"
+
+                if invalid_path_file.exists():
+                    with invalid_path_file.open(mode="r") as f:
+                        records = json.load(f)
+
+                    records.append(resource)
+
+                else:
+                    records = [resource]
+
+                with invalid_path_file.open(mode="w") as f:
+                    json.dump(records, f)
