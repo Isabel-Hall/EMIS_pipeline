82ba964de27ebfefc4949fad3e2ff5501cf4be1c
i_hall <isabel.m.hall@gmail.com>
2022-02-14 14:05:46 +0000

    takes a directory rather than individual json file

--- process_bundles.py
+++ process_bundles.py
@@ -3,50 +3,50 @@ from pathlib import Path
 import fire
 
 
-def process_bundles(json_file_path):
+def process_bundles(dir_path):
     """
-    Takes an individual json file containing FHIR data
+    Takes an directory of json files containing FHIR data
     Separates it based on resource types and saves these into json files
     """
+    input_dir = Path(dir_path)
+    for p in input_dir.iterdir():
+        print("opening file:", p)
+        # Open the FHIR json file
+        with p.open(mode="r") as f:
+            bundle = json.load(f)
+            entries = bundle["entry"]
 
-    # Open the FHIR json file
-    with open(json_file_path, "r") as f:
-        bundle = json.load(f)
-        entries = bundle["entry"]
 
+        resource_types = set()
 
-    resource_types = set()
+        # Make a directory to save the processed files
+        output_dir = Path("resources")
+        if not output_dir.exists():
+            output_dir.mkdir()
 
-    # Make a directory to save the processed files
-    output_dir = Path("resources")
-    if not output_dir.exists():
-        output_dir.mkdir()
+        # Group different resource types together and save them into distinct json files
+        for resource in entries:
+            resource_type = resource["resource"]["resourceType"]
+            resource_types.add(resource_type)
+            resource_file_path = output_dir / f"{resource_type}.json"
 
-    # Group different resource types together and save them into distinct json files
-    for resource in entries:
-        resource_type = resource["resource"]["resourceType"]
-        resource_types.add(resource_type)
-        resource_file_path = output_dir / f"{resource_type}.json"
+            if resource_file_path.exists():
 
-        if resource_file_path.exists():
-
-            # get existing records of this resource
-            with resource_file_path.open(mode="r") as f:
-                records = json.load(f)
-        
-            # add stuff to it
-            records.append(resource)
-
-        else:
-            records = [resource]
+                # get existing records of this resource
+                with resource_file_path.open(mode="r") as f:
+                    records = json.load(f)
             
-        # save it to disk
-        with resource_file_path.open(mode="w") as f:
-            json.dump(records, f)
+                # add stuff to it
+                records.append(resource)
 
+            else:
+                records = [resource]
+                
+            # save it to disk
+            with resource_file_path.open(mode="w") as f:
+                json.dump(records, f)
 
-
-    #print(resource_types)
+        #print(resource_types)
 
 if __name__ == "__main__":
     fire.Fire(process_bundles)
