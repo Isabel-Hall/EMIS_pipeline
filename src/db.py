9b25487a787f0cac77f1119b7eca6a09bf80dba4
i_hall <isabel.m.hall@gmail.com>
2022-02-15 09:12:13 +0000

    add files to db, runs from cmd with fire

--- src/db.py
+++ src/db.py
@@ -3,50 +3,53 @@ import os
 import json
 from pathlib import Path
 from tqdm import tqdm
+import fire
 
-def get_db_details():
-    # Initialise a mongoDB client
-    mongo_url = os.environ["MONGO_URL"]
-    mongo_username = os.environ["MONGO_INITDB_ROOT_USERNAME"]
-    mongo_password = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
-    client = pymongo.MongoClient(
-        mongo_url,
-        username=mongo_username,
-        password=mongo_password
-    )
-    db = client.fhir_data
-    return db
+class createDB():
 
-def add_collection(db, file_path):
+    def __init__(self):
+        # Initialise a mongoDB client
+        mongo_url = os.environ["MONGO_URL"]
+        mongo_username = os.environ["MONGO_INITDB_ROOT_USERNAME"]
+        mongo_password = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
+        client = pymongo.MongoClient(
+            mongo_url,
+            username=mongo_username,
+            password=mongo_password
+        )
+        self.db = client.fhir_data
+        
 
-    # Open a json file containing fhir resources, of one resource type
-    print(file_path)
-    with file_path.open(mode="r") as f:
-        resources = json.load(f)
+    def add_collection(self, file_path):
 
-    records_to_save = []
+        # Open a json file containing fhir resources, of one resource type
 
-    # Extract the resource data and add the fullUrl, removes one layer of nesting
-    for resource in resources:
-        res = resource["resource"]
-        url = resource["fullUrl"]
-        res["fullUrl"] = url
-        records_to_save.append(res)
-    
-    # Insert into mongoDB client
-    name = file_path.stem
-    inserted = db[name].insert_many(records_to_save)
-    return inserted
+        with file_path.open(mode="r") as f:
+            resources = json.load(f)
 
-def add_many_collections(db, path_dir):
-    path = Path(path_dir)
-    file_paths = list(path.glob("*.json"))
-    for p in tqdm(file_paths, total = len(file_paths)):
-        inserted = add_collection(db, p)
+        records_to_save = []
+
+        # Extract the resource data and add the fullUrl, removes one layer of nesting
+        for resource in resources:
+            res = resource["resource"]
+            url = resource["fullUrl"]
+            res["fullUrl"] = url
+            records_to_save.append(res)
+        
+        # Insert into mongoDB client
+        name = file_path.stem
+        inserted = self.db[name].insert_many(records_to_save)
+        return inserted
+
+    def add_many_collections(self, path_dir):
+        path = Path(path_dir)
+        file_paths = list(path.glob("*.json"))
+        for p in tqdm(file_paths, total = len(file_paths)):
+            inserted = self.add_collection(p)
 
 
 if __name__ == "__main__":
-    db = get_db_details()
-    file_path = "../validated_resources/"
+    #db = get_db_details()
+    #file_path = "../validated_resources/"
 
-    add_many_collections(db, file_path)
+    fire.Fire(createDB)
